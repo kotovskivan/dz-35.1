002519629f2824e6cc7dac8aecdcf40e97459fb4
Kotovski <workkotovski@gmail.com>
2025-09-20 10:12:23 +0300

    add dist build

---
+++ dist/js/main.5438cfe2.js
@@ -0,0 +1,31 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkwebpack_advanced_seed"] = self["webpackChunkwebpack_advanced_seed"] || []).push([[792],{
+
+/***/ 511:
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "images/logo.2d6de051.png";
+
+/***/ }),
+
+/***/ 103:
+/***/ (function(__unused_webpack___webpack_module__, __unused_webpack___webpack_exports__, __webpack_require__) {
+
+eval("\nconst el = document.getElementById(\"app\");\nconst img = new Image();\nimg.src = new URL(/* asset import */ __webpack_require__(511), __webpack_require__.b);\nimg.alt = \"logo\";\nimg.className = \"logo\";\nconst btn = document.createElement(\"a\");\nbtn.className = \"btn\";\nbtn.href = \"#\";\nbtn.textContent = _.join([\"Hello\", \"Webpack\"], \" \");\nel.append(img, btn);\nif (false) {}//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMTAzLmpzIiwibWFwcGluZ3MiOiJBQUFBO0FBRUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFFQSIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYnBhY2stYWR2YW5jZWQtc2VlZC8uL3NyYy9pbmRleC5qcz9iNjM1Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBcIi4vc3R5bGVzL21haW4uY3NzXCJcclxuXHJcbmNvbnN0IGVsID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJhcHBcIilcclxuY29uc3QgaW1nID0gbmV3IEltYWdlKClcclxuaW1nLnNyYyA9IG5ldyBVUkwoXCIuL2Fzc2V0cy9pbWFnZXMvbG9nby5wbmdcIiwgaW1wb3J0Lm1ldGEudXJsKVxyXG5pbWcuYWx0ID0gXCJsb2dvXCJcclxuaW1nLmNsYXNzTmFtZSA9IFwibG9nb1wiXHJcbmNvbnN0IGJ0biA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoXCJhXCIpXHJcbmJ0bi5jbGFzc05hbWUgPSBcImJ0blwiXHJcbmJ0bi5ocmVmID0gXCIjXCJcclxuYnRuLnRleHRDb250ZW50ID0gXy5qb2luKFtcIkhlbGxvXCIsIFwiV2VicGFja1wiXSwgXCIgXCIpXHJcbmVsLmFwcGVuZChpbWcsIGJ0bilcclxuXHJcbmlmIChpbXBvcnQubWV0YS53ZWJwYWNrSG90KSB7XHJcbiAgaW1wb3J0Lm1ldGEud2VicGFja0hvdC5hY2NlcHQoKVxyXG59XHJcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///103\n");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ var __webpack_exports__ = (__webpack_exec__(103));
+/******/ }
+]);
